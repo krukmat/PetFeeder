6ba7731fa44ce1961139a3ab453e8d67def5673d
Matias Kruk <kruk.matias@gmail.com>
2022-10-02 11:43:39 +0200

    changes in zetup

--- PetFeeder.h
+++ PetFeeder.h
@@ -2,17 +2,16 @@
 const char* ssid = "MIWIFI_2G_2jJ5";
 const char* pass = "xvFYmqRv";
 
-const int MOIST_SENSOR = 34;
-const int RELAY = 14;
+// Template ID, Device Name and Auth Token are provided by the Blynk.Cloud
+// See the Device Info tab, or Template settings
+#define BLYNK_TEMPLATE_ID           "TMPLMU2E491r"
+#define BLYNK_DEVICE_NAME           "Quickstart Device"
+#define BLYNK_AUTH_TOKEN            "h0vG7RE_aqJVxvvcsvAUdkgD1IiNk0nJ"
 
-const char* mqtt_ip = "ioticos.org";
-const int mqtt_ip_port = 1883;
-const char* mqtt_ip_user = "d2uNYOZq2J5uL3T";
-const char* mqtt_ip_password = "DwqGBXa7LuVlgBG";
-const char *mqtt_ip_topic = "HPIbCG0C72lcw6g/output";
-const char *mqtt_ip_topic_subscribe = "HPIbCG0C72lcw6g/input";
-const int MQTT_RETRYMS = 5000;
 
-char auth[] = "yEWTTtrrde6_IQxbi4L6TkP9bh_lCD0y";
-int readMs = 90000;
-int sensorStatus = 0;
+// Comment this out to disable prints and save space
+#define BLYNK_PRINT Serial
+
+char auth[] = BLYNK_AUTH_TOKEN;
+
+int servoPin = 2;
--- PetFeeder.ino
+++ PetFeeder.ino
@@ -1,73 +1,36 @@
 
 #include "PetFeeder.h"
+#include <WiFi.h>
+#include <WiFiClient.h>
 #include <BlynkSimpleEsp32.h>
 #include <ESP32Servo.h>
-#include <WiFi.h>
-#include <HTTPClient.h>
-#include <PubSubClient.h>
    
-
-WiFiClient mqttIPClientWifi;
-PubSubClient mqttIPClient( mqttIPClientWifi );
-const String deviceId = "PLANT_1";
-
-#include <mqtt.h>
-
-
-
-
-// You should get Auth Token in the Blynk App.
-// Go to the Project Settings (nut icon).
 
 Servo myservo;  // create servo object to control a servo
  
-// Recommended PWM GPIO pins on the ESP32 include 2,4,12-19,21-23,25-27,32-33 
-int servoPin = 2;
-
 
 // This function will be called every time Slider Widget
-// in Blynk app writes values to the Virtual Pin 1
-void feed_rabbit()
+// in Blynk app writes values to the Virtual Pin 0
+BLYNK_WRITE(V0)
 {
   myservo.write(0); 
   delay(200);
-  myservo.write(180);
+  myservo.write(95);
 }
 
 void blynk_setup(){
   Blynk.begin(auth, ssid, pass);
 }
 
-void mqttCallback(char* topic, byte* payload, unsigned int length){
-  String parameter = getCommand(deviceId, topic, payload, length);
-    if (parameter == "execute"){
-       Serial.println("feed rabbit method");
-       feed_rabbit();
-  }
-}
-
 void setup()
 {
   Serial.begin(9600);
-  //blynk_setup();
+  blynk_setup();
   myservo.setPeriodHertz(50); 
   myservo.attach(servoPin);
-  WiFi.begin(ssid, pass);
-
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("");
-  Serial.println("WiFi connected");
-  
-  mqttIPClient.setServer(mqtt_ip, mqtt_ip_port);
-  mqttIPClient.setCallback(mqttCallback);
-  bootUp(deviceId, &mqttIPClient, mqtt_ip, mqtt_ip_port, mqtt_ip_user, mqtt_ip_password, mqtt_ip_topic_subscribe, mqtt_ip_topic, MQTT_RETRYMS);
 }
 
 void loop()
 {
-  mqttLoop(&mqttIPClient, mqtt_ip_user, mqtt_ip_password, mqtt_ip_topic_subscribe, mqtt_ip_topic, MQTT_RETRYMS);
-  //Blynk.run();
+  Blynk.run();
 }
